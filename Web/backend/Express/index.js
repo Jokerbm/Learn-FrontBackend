f76bbc3b76138a637a33615856a98bef54908430
ryu <ryuchotichan@gmail.com>
2019-11-22 20:38:25 +0700

    52 minute

--- Web/backend/Express/index.js
+++ Web/backend/Express/index.js
@@ -5,20 +5,15 @@ const logger = require('./middleware/logger');
 //path
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-//database
-const members = require('./Members')
-//path
-
 //init middleware
 app.use(logger)
-
-//all members
-app.get('/api/members', (req, res) => {
-    res.json(members)
-})
-
-//set static folde
+//body parser middle ware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
+//set static folder
 app.use(express.static(path.join(__dirname, 'public')))
+//members api route
+app.use('/api/members', require('./routes/api/members'))
 app.listen(PORT, () => console.log(`server Start on port ${PORT}`));
 //routes for 1 index
 // app.get('/', (req, res) => {
---
+++ Web/backend/Express/routes/api/members.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const uuid = require('uuid')
+//database
+const members = require('../../Members')
+//all members
+router.get('/', (req, res) => {
+    res.json(members)
+})
+//single members
+router.get('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)))
+    }
+    else {
+        res.status(400).json({ msg: `member id ${req.params.id} is not found` })
+    }
+})
+
+//create members
+router.post('/', (req, res) => {
+    // res.send(req.body) check
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    }
+    if(!newMember.name || !newMember.email){
+        return res.status(400).json({msg: `Please include name and email`})
+    }
+
+    members.push(newMember);
+    res.json(members)
+});
+// update members
+router.put('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+    if (found) {
+        const updMember = req.body;
+        members.forEach(member =>{
+            if(member.id === parseInt(req.params.id)){
+                member.name = req.body.name;
+                member.email = req.body.email;
+            }
+        })
+    }
+    else {
+        res.status(400).json({ msg: `member id ${req.params.id} is not found` })
+    }
+})
+module.exports = router;
