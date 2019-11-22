d8a421132c17e15af2292b45495a4cd1f2821803
ryu <ryuchotichan@gmail.com>
2019-11-22 17:40:55 +0700

    express staticpath

--- Web/backend/Express/index.js
+++ Web/backend/Express/index.js
@@ -1,8 +1,14 @@
 // use expresss
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+//path
+//set static folde
+
+const path = require('path');
 const PORT = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, 'public')))
 app.listen(PORT, () => console.log(`server Start on port ${PORT}`));
+//routes for 1 index
+// app.get('/', (req, res) => {
+//     res.sendFIle(path.join(__dirname, 'public', 'index.html'))
+// })
