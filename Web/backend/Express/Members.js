3c9a6a77fef079dc3c5adae539193785347b5834
ryu <ryuchotichan@gmail.com>
2019-11-22 19:39:17 +0700

    Findbug
    app2throw

---
+++ Web/backend/Express/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'ddd',
+        email: 'aaaaa@a',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'ddd',
+        email: 'aaaaa@a',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'ddd',
+        email: 'aaaaa@a',
+        status: 'active'
+    }
+]
+
+module.exports = members;
--- Web/backend/Express/index.js
+++ Web/backend/Express/index.js
@@ -1,11 +1,23 @@
 // use expresss
 const express = require('express');
 const app = express();
+const logger = require('./middleware/logger');
 //path
-//set static folde
-
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+//database
+const members = require('./Members')
+//path
+
+//init middleware
+app.use(logger)
+
+//all members
+app.get('/api/members', (req, res) => {
+    res.json(members)
+})
+
+//set static folde
 app.use(express.static(path.join(__dirname, 'public')))
 app.listen(PORT, () => console.log(`server Start on port ${PORT}`));
 //routes for 1 index
