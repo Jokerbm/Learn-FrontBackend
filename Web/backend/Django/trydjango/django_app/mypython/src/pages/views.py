5db3c144503450e8675a1ecf0c6e2c7864009fbe
ryu <ryuchotichan@gmail.com>
2020-02-07 10:54:24 -0500

    Django temblate and base.html

--- Web/backend/Django/trydjango/django_app/mypython/src/pages/views.py
+++ Web/backend/Django/trydjango/django_app/mypython/src/pages/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def home_view(request, *args, **kwargs):
-    print(request)
-    print(request.user)
-    return HttpResponse("<h1>Hello world</h1>")
+    return render(request, "home.html", {})
+    # return HttpResponse("<h1>Hello world</h1>")
+
 
 def contact_view(*args, **kwargs):
     return HttpResponse("<h1>COntactpages</h1>")
