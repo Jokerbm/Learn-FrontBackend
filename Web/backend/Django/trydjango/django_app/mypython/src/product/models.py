2a9b51ca018896551272506dda55dce9fcb3c70a
ryu <ryuchotichan@gmail.com>
2020-02-11 14:46:54 -0500

    hacking branch

--- Web/backend/Django/trydjango/django_app/mypython/src/product/models.py
+++ Web/backend/Django/trydjango/django_app/mypython/src/product/models.py
@@ -6,4 +6,4 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=1000)
     summary = models.TextField(default = 'this is cool')
-    feature = models.BooleanField() #readdocs about require ment
+    feature = models.BooleanField() #readdocs about require max_length
