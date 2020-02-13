a91539efbfa4d24bc4dddd4f7922c17be34a4806
ryu <ryuchotichan@gmail.com>
2020-02-13 06:23:36 -0500

    END oF JS crash course

--- Web/FrontENd/Javascript/js for beginner/main.js
+++ Web/FrontENd/Javascript/js for beginner/main.js
@@ -1,3 +1,24 @@
-const ul = document.querySelector('.items')
+const myForm = document.querySelector('#my-form')
+const nameInput = document.querySelector('#name')
+const emailInut = document.querySelector('#email')
+const msg = document.querySelector('.msg')
+const userList = document.querySelector('#users')
 
-ul.remove()
+myForm.addEventListener('submit', onsubmit)
+
+function onsubmit (e) {
+	e.preventDefault()
+	if(nameInput.value === '' || emailInut.value === ''){
+		msg.classList.add('error')
+		msg.innerHTML = 'please enter all fields'
+		setTimeout(() => msg.remove(), 3000)
+	}
+	else {
+		const li = document.createElement('li')
+		li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInut.value}`))
+		userList.appendChild(li)
+		nameInput.value = ''
+		emailInut.value = ''
+	}
+	// body... 
+}
