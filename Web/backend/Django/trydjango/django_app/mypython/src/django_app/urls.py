c6ffe1254260e9762ddc4082b35ee3c43da65802
ryu <ryuchotichan@gmail.com>
2020-02-18 22:46:11 -0500

    FIXpromblem(django)

--- Web/backend/Django/trydjango/django_app/mypython/src/django_app/urls.py
+++ Web/backend/Django/trydjango/django_app/mypython/src/django_app/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import *
+from product.views import product_detail_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('contact/', contact_view, name='contact'),
-    path('about/', about_view, name='about'),
+    path('about/', about_view),
+    path('product/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
