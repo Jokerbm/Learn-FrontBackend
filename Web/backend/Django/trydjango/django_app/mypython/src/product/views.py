ba90c2ac9ad99ce5f2c350c9e2f0b36952efa91c
ryu <ryuchotichan@gmail.com>
2020-02-18 23:19:51 -0500

    Django model_form

--- Web/backend/Django/trydjango/django_app/mypython/src/product/views.py
+++ Web/backend/Django/trydjango/django_app/mypython/src/product/views.py
@@ -6,8 +6,11 @@ from .models import Product
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
+    # my_context = {
+    #     'title': obj.title,
+    #     'description': obj.description
+    # }
     my_context = {
-        'title': obj.title,
-        'description': obj.description
+        'object': obj
     }
-    return render(request, "product/detail.html", my_context)
+    return render(request, "products/product_detail.html", my_context)
