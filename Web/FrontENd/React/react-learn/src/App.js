180c1cde4fcd73516021fea5982756cfaf2e0312
ryu <ryuchotichan@gmail.com>
2020-02-22 18:03:50 -0500

    Dinner Break

--- Web/FrontENd/React/react-learn/src/App.js
+++ Web/FrontENd/React/react-learn/src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import TodoItem from './TodoItem';
 import todosData from './todosdata';
 
-function App() {
-  const todoItems = todosData.map(item => (
-    <TodoItem key={item.id} item={item} />
-  ));
-  return <div className="todo-list">{todoItems}</div>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+  }
+  render() {
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} />
+    ));
+    return <div className="todo-list">{todoItems}</div>;
+  }
 }
 export default App;
