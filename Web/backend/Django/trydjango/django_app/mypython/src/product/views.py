2ff98993b4b2b1114a77e59e436665d238163014
ryu <ryuchotichan@gmail.com>
2020-03-10 12:40:18 -0400

    django break battery down

--- Web/backend/Django/trydjango/django_app/mypython/src/product/views.py
+++ Web/backend/Django/trydjango/django_app/mypython/src/product/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 from .models import Product
 # Create your views here.
 
 def product_create_view(request):
+    my_form = RawProductForm()
     context = {
+        "form": my_form
     }
     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+#     # print(request.GET)
+#     # print(request.POST)
+#     if request.method == 'POST':
+#         m_new_title = request.POST.get('title')
+#         print(m_new_title)
+#     #product.object.create(title)
+#     context = {
+#     }
+#     return render(request, "products/product_create.html", context)
 
 # def product_create_view(request):
 #     form = ProductForm(request.POST or None)
